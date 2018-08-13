da382dd5535893b2ee4edcd65dd1a9d533000f79
Arjen Rouvoet <a.j.rouvoet@gmail.com>
2018-08-13 15:14:52 +0200

    Update to Agda 2.5.4

--- default.nix
+++ default.nix
@@ -6,18 +6,18 @@ let
   # get nixplus
   nixplus = import (pkgs.fetchgit {
     url    = https://gitlab.ewi.tudelft.nl/aj.rouvoet/nixplus.nix.git;
-    sha256 = "1qmln4c0vla5an4vmkf828q7xg8qkbfrlpmhg0kjb064nvnrcr0h";
-    rev    = "fda1611c6263774f8e9ca31eab3e1c6ba778d505";
+    sha256 = "0zla8sf6k1n0ggb2h416n0x4ganjr3lnrz4mv5blsfyakjhrrxha";
+    rev    = "e9d6d27e37e88d318dd5963b1751482c292ce36f";
   });
 
 in nixplus.stdenv.mkDerivation rec {
-  version = "1.0.0";
+  version = "1.1.0";
   name    = "mj.agda-${version}";
   src     = ./.;
 
   buildInputs = with nixplus; [
     git
-    agdaPackages.Agda.v2_6_0
+    agdaPackages.Agda.v2_5_4
   ];
 }
 
